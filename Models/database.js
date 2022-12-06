4b5890c8ab45186993813ed31f6cb7e49b74bb61
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2022-12-06 22:39:15 +0330

    change pass

--- Models/database.js
+++ Models/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-module.exports = new Sequelize("todo_db", 'root', '0691001472!', {
+module.exports = new Sequelize("todo_db", 'root', '123456', {
     dialect: 'mysql',
     host: 'localhost'
 });
